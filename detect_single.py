cccf352d5d32189620c718d048cf1b7462701f2d
Alexandru Dinu <alex.dinu07@gmail.com>
2018-07-09 17:14:34 +0300

    add area threshold

--- detect_single.py
+++ detect_single.py
@@ -15,6 +15,7 @@ parser.add_argument('--data_config_path', type=str, default='config/coco.data', 
 parser.add_argument('--weights_path', type=str, default='weights/yolov3.weights', help='path to weights file')
 parser.add_argument('--conf_thres', type=float, default=0.8, help='object confidence threshold')
 parser.add_argument('--nms_thres', type=float, default=0.4, help='iou thresshold for non-maximum suppression')
+parser.add_argument('--area_thres', type=float, default=0, help='ignore objs with area < threshold')
 parser.add_argument('--use_cuda', action="store_true", help='whether to use cuda if available')
 opt = parser.parse_args()
 
@@ -64,6 +65,14 @@ while True:
 		detections = non_max_suppression(detections, num_classes, opt.conf_thres, opt.nms_thres)
 		detections = detections[0]
 
+	filtered_detections = []
+	for d in detections:
+		x1, y1, x2, y2 = Tensor(d[:4]).view(1, -1)[0]
+		area = (x2 - x1) * (y2 - y1) / (img_size * img_size)
+		if area >= opt.area_thres:
+			filtered_detections.append(d)
+	detections = torch.stack(filtered_detections)
+
 	img = cv2.imread(path)
 
 	# The amount of padding that was added
@@ -80,6 +89,8 @@ while True:
 		bbox_colors = random.sample(COLORS, len(unique_labels))
 
 		for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
+			area = (x2 - x1) * (y2 - y1) / (img_size * img_size)
+
 			# rescale coordinates to original dimensions
 			x1 = ((x1 - pad_x // 2) / unpad_w) * img.shape[1]
 			y1 = ((y1 - pad_y // 2) / unpad_h) * img.shape[0]
@@ -89,14 +100,14 @@ while True:
 			color = bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
 
 			# draw bbox over image
-			cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
+			cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
 
 			# add label
 			cv2.putText(img, str(int(cls_pred)), (x1, y1 - 3), FONT, 1, (255, 255, 255), 1)
 
 			print(
-				'\t+ Coords: [%4d, %4d, %4d, %4d], Class: %s, ObjConf: %.5f, ClassProb: %.5f' % \
-				(x1, y1, x2, y2, classes[int(cls_pred)], conf.item(), cls_conf.item())
+				'\t+ Coords: [%4d, %4d, %4d, %4d], Class: %s, ObjConf: %.5f, ClassProb: %.5f, Area: %.5f' % \
+				(x1, y1, x2, y2, classes[int(cls_pred)], conf.item(), cls_conf.item(), area)
 			)
 
 	# Save generated image with detections
