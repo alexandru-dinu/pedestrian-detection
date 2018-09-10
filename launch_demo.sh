b6b73b192b6db2767d73943945246affbb1298b7
Alexandru Dinu <alex.dinu07@gmail.com>
2018-09-11 00:17:25 +0300

    updates

--- launch_demo.sh
+++ launch_demo.sh
@@ -4,13 +4,13 @@ CFGS="./cfgs"
 WEIGHTS="./weights"
 
 
-CUDA_VISIBLE_DEVICES=$1 python detect.py \
+CUDA_VISIBLE_DEVICES=$1 python demo.py \
     --model_config_path ${CFGS}/$2/yolov3-$2-test.cfg \
     --data_config_path ${CFGS}/$2/$2.data \
     --weights_path ${WEIGHTS}/$2/yolov3-$2_final.weights \
     --n_cpu 1 \
-    --detect_dir bdd1 \
+    --detect_dir /home/alex/Desktop/bdd1_out \
     --conf_thres 0.6 \
     --nms_thres 0.4 \
     --use_cuda \
-    --batch_count 10
+    --batch_count -1
